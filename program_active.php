1dce191cc80d525a2d9ef69f4a7cae3094738ae9
Marius Mandal <mariusmandal@gmail.com>
2014-04-05 08:41:25 +0200

    test

--- program_active.php
+++ program_active.php
@@ -3,8 +3,37 @@
 require_once('UKM/monstring.class.php');
 require_once('UKM/forestilling.class.php');
 
+$perioder = array();
+
 $m = new monstring( get_option('pl_id') );
 
-$hendelser = $m->hendelser();
+$hendelser = $m->forestillinger();
 
-var_dump( $hendelser );
+foreach( $hendelser as $c ) {
+	$h = new forestilling( $c['c_id'] );
+	$innslag = $h->innslag();
+	$start = $h->get('c_start');
+	
+	// Beregn varighet og stopp-tidspunkt
+	$varighet = $forestilling->varighet();
+	if( $varighet <= 60*30 ) {
+		$varighet = 60*45;
+	} else {
+		$varighet = $varighet * 1.1;
+	}
+	$stop = $start + $varighet;
+	
+	// Start 8 min før
+	$varighet += 60*8;
+	$start = $start - 60*8;
+	
+	$perioder[] = (object) array('start' => $start, 'stop' => $stop);
+}
+
+var_dump( $perioder );
+
+foreach( $perioder as $i => $data ) {
+	echo '<h2>'. $i .'</h2>';
+	echo 'start: '. date('d.m.Y H:i:s', $data->start)
+		.'stop: '. date('d.m.Y H:i:s', $data->stop);
+}
