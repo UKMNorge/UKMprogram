00b2c0be30474f81f34b19240c8849837526c49d
Marius Mandal <mariusmandal@gmail.com>
2014-04-05 08:45:22 +0200

    debug

--- program_active.php
+++ program_active.php
@@ -13,27 +13,31 @@ foreach( $hendelser as $c ) {
 	$h = new forestilling( $c['c_id'] );
 	$innslag = $h->innslag();
 	$start = $h->get('c_start');
+	$navn = $h->get('c_name');
 	
 	// Beregn varighet og stopp-tidspunkt
 	$varighet = $h->varighet();
+/*
 	if( $varighet <= 60*30 ) {
 		$varighet = 60*45;
 	} else {
 		$varighet = $varighet * 1.1;
 	}
+*/
 	$stop = $start + $varighet;
 	
 	// Start 8 min før
 #	$varighet += 60*8;
 #	$start = $start - 60*8;
 	
-	$perioder[] = (object) array('start' => $start, 'stop' => $stop);
+	$perioder[] = (object) array('navn' => $navn, 'start' => $start, 'stop' => $stop, 'varighet' => $varighet);
 }
 
 var_dump( $perioder );
 
 foreach( $perioder as $i => $data ) {
-	echo '<h2>'. $i .'</h2>';
+	echo '<h2>'. $i .' - '. $data->navn.'</h2>';
 	echo 'start: '. date('d.m.Y H:i:s', $data->start)
-		.'stop: '. date('d.m.Y H:i:s', $data->stop);
+		.'stop: '. date('d.m.Y H:i:s', $data->stop)
+		.'varig: '. $data->varighet;
 }
