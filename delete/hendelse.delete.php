74bc167a0a4062d2aba5e91821ed7e3641237bf2
Marius Mandal <mariusmandal@gmail.com>
2019-12-13 14:29:32 +0100

    init logger for slett

--- delete/hendelse.delete.php
+++ delete/hendelse.delete.php
@@ -3,6 +3,8 @@
 use UKMNorge\Arrangement\Arrangement;
 use UKMNorge\Arrangement\Program\Write;
 
+UKMprogram::setupLogger();
+
 $arrangement = new Arrangement(get_option('pl_id'));
 $hendelse = $arrangement->getProgram()->get( $_GET['c_id'] );
 
