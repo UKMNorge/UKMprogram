1681d081dc3e9b1f334a9e41756049f82ca8c82b
Marius Mandal <mariusmandal@gmail.com>
2019-12-16 21:41:58 +0100

    fjernet v1 🥳🥳🥳🥳🥳🥳🥳🥳🥳

--- old/ajax/save/fjern_fra_forestilling.save.php
+++
@@ -1,9 +0,0 @@
-<?php
-function UKMprogram_save($info){
-	require_once('UKM/forestilling.class.php');
-	$c = new forestilling($info['c_id']);
-	$res = $c->fjern($info['b_id']);
-	do_action('UKMprogram_save', 'fjern_fra_forestilling', $info['c_id'], $info['b_id']);
-	die(json_encode(array('result'=>$res)));
-}
-?>
