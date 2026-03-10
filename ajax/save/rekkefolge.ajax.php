4c730c386b607a573c6e0c6791dec75ed5882649
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2026-03-10 13:46:35 +0100

    Visning og drag and drop aktivitet
    Husk DB smartukm_rel_aktivitet_c

--- ajax/save/rekkefolge.ajax.php
+++ ajax/save/rekkefolge.ajax.php
@@ -1,15 +1,26 @@
 <?php
 
 use UKMNorge\Arrangement\Arrangement;
+use UKMNorge\Arrangement\Aktivitet\Aktivitet;
 use UKMNorge\Arrangement\Program\Write;
+use UKMNorge\Innslag\Innslag;
 
 require_once('UKM/Autoloader.php');
 
-
 $arrangement = new Arrangement( get_option('pl_id') );
 $hendelse = $arrangement->getProgram()->get( $_POST['hendelse'] );
-$rekkefolge = explode(',',$_POST['innslag']);
-Write::redefineOrder( $hendelse, $rekkefolge );
+$rekkefolgeData = json_decode(stripslashes($_POST['rekkefolge']), true);
 
-// Sett til true, fordi try-catch vil sette false hvis write feiler
+
+$items = [];
+foreach ($rekkefolgeData as $entry) {
+    if ($entry['type'] === 'innslag') {
+        $items[] = new Innslag((int) $entry['id']);
+    } elseif ($entry['type'] === 'aktivitet') {
+        $items[] = new Aktivitet((int) $entry['id']);
+    }
+}
+
+Write::redefineOrder( $hendelse, $items );
+
 UKMprogram::addResponseData('success', true);
