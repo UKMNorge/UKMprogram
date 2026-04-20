8272c543d74c85ca9f7f3227910b704578a76380
Jardar Nordbo <jardar@ukm.no>
2026-04-20 07:51:01 +0000

    Reverser rekkefølge virker nå

--- js/reverser.js
+++ js/reverser.js
@@ -1,13 +1,20 @@
 jQuery(document).ready(function() {
     jQuery(".reverser_rekkefolge").click(function(clickEvent) {
         clickEvent.preventDefault();
+        var hendelseId = jQuery(clickEvent.currentTarget).data('hendelse');
+
+        if (!hendelseId) {
+            alert('Klarte ikke å finne hendelsen som skulle reverseres.');
+            return;
+        }
+
         if(confirm('Er du sikker på at du vil reversere rekkefølgen på alle innslag i denne hendelsen?')) {
             jQuery.post(
                 ajaxurl, {
                     action: 'UKMprogramV2_ajax',
                     controller: 'save',
                     save: 'reverserRekkefolge',
-                    hendelse: jQuery(clickEvent.target).data('hendelse')
+                    hendelse: hendelseId
                 },
                 function(response) {
                     if (response !== null && response.success) {
